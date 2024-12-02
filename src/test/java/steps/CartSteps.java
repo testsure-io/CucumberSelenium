c652a1015fa257cc96ca2013fafda0012ec62dcc
Vithika911 <yash@testsure.io>
2024-12-02 11:11:00 +0530

    succefully upload

---
+++ src/test/java/steps/CartSteps.java
@@ -0,0 +1,47 @@
+package steps;
+
+import io.cucumber.java.en.Then;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class CartSteps extends BaseSteps{
+
+    @Then("Add To Cart")
+    public void addToCart() {
+        WebElement AddToCart = driver.findElement(By.cssSelector(".btn_primary.btn_inventory"));
+        AddToCart.click();
+
+    }
+
+    @Then("go to Shopping Cart")
+    public void goToShoppingCart() {
+        driver.findElement(By.id("shopping_cart_container")).click();
+
+    }
+
+    @Then("click Checkout button")
+    public void clickCheckoutButton() {
+        driver.findElement(By.cssSelector(".btn_action.checkout_button")).click();
+    }
+
+    @Then("Enter Checkout Details")
+    public void enterCheckoutDetails() {
+        driver.findElement(By.id("first-name")).sendKeys("Vithika");
+
+        driver.findElement(By.id("last-name")).sendKeys("patel");
+        driver.findElement(By.id("postal-code")).sendKeys("310091");
+
+        WebElement btn = driver.findElement(By.cssSelector(".btn_primary.cart_button"));
+        btn.click();
+        WebElement btn1 = driver.findElement(By.cssSelector(".btn_action.cart_button"));
+        btn1.click();
+        driver.close();
+
+
+    }
+}
+
